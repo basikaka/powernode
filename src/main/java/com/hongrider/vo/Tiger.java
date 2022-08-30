fbad360c0de94395477b1c6da827e056621ef736
basikaka <hellochenpro@hotmail.com>
2022-08-30 17:50:10 +0800

    Initial commit

---
+++ src/main/java/com/hongrider/vo/Tiger.java
@@ -0,0 +1,22 @@
+package com.hongrider.vo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("whitetiger")
+public class Tiger {
+
+    @Value("${tiger.name}")
+    private String name;
+
+    @Value("${tiger.age}")
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "Tiger{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/test/java/com/hongrider/vo/MyTest.java
@@ -0,0 +1,60 @@
+package com.hongrider.vo;
+
+import com.hongrider.config.SpringConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class MyTest {
+
+    /**
+     * 使用xml文件配置容器对象
+     */
+    @Test
+    void Test01(){
+        String config = "beans.xml";
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext( config );
+        System.out.println("容器中的对象： " + ctx.getBean( "goodStudent"));
+    }
+
+    /*
+    * 使用JavaConfig
+    * */
+    @Test
+    void Test02(){
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        System.out.println("使用JavaConfig创建的Bean，容器中的对象： " + ctx.getBean("sunCat"));
+    }
+
+    @Test
+    void Test03(){
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        Student student = (Student) ctx.getBean("hill");
+        System.out.println("使用JavaConfig创建的Bean，容器中的对象： " + student);
+    }
+
+    /*
+    * 使用ImportResource 导入xml文件配置的bean对象
+    **/
+    @Test
+    void Test04(){
+        ApplicationContext ctx = new AnnotationConfigApplicationContext( SpringConfig.class);
+        System.out.println("使用ImportResource导入xml配置bean的容器对象：" + ctx.getBean("gorCat"));
+    }
+
+    @Test
+    void Test05(){
+        ApplicationContext ctx = new AnnotationConfigApplicationContext( SpringConfig.class);
+        System.out.println("使用propertysoruce 创建Bean对象：" + ctx.getBean("whitetiger"));
+    }
+
+
+
+}
