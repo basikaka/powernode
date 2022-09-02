a4f172a426832cba5daa06418aafb36d4cb90f55
basikaka <hellochenpro@hotmail.com>
2022-09-02 12:56:02 +0800

    使用commandlinerunner接口，获取容器对象

---
+++ springboot-commandlinerunner-011/src/main/java/com/hongrider/service/HelloService.java
@@ -0,0 +1,5 @@
+package com.hongrider.service;
+
+public interface HelloService {
+    void sayHello( String name);
+}
---
+++ springboot-commandlinerunner-011/src/test/java/com/hongrider/SpringbootCommandlinerunner011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongrider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommandlinerunner011ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
