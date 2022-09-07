e23194f5d358c2a519b652d0b026eb0f8b8344a2
basikaka <hellochenpro@hotmail.com>
2022-09-07 19:14:17 +0800

    添加charater filter

---
+++ springboot_character_filter_015/src/main/java/com/hogrider/characterfilter015application.java
@@ -0,0 +1,13 @@
+package com.hogrider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class characterfilter015application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(characterfilter015application.class, args);
+    }
+
+}
